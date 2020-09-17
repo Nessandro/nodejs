2332ab1fd73efe18efcc6090bd3926693baeb579
Tomasz Bielecki <bieleckitomasz94@gmail.com>
2020-09-17 18:09:39 +0200

    =#add morgan logger and

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const path   = require('path')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
-
-
-//load config
+//load config firstly
 dotenv.config({path:  path.join(__dirname,'config','config.env')})
 
+//connect with DB
+connectDB();
+
+/* init app with express*/
 const app = express();
 
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+/* get port from env or 3000 as default*/
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>{
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-})
+/* start listening on app */
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
+
+//to start the app run `npm run dev` (npm run start) in console
